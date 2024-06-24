e494e53a55e0821ca544578c7694d866f3a008c4
mymain838 <hades517111@gmail.com>
2024-06-24 23:36:40 +0900

    개발용 브랜치

--- src/components/ImageUpload.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-const ImageUpload = (image) => {
-  const [preview, setPreview] = useState("");
-
-  useEffect(() => {}, []);
-};
-
-export default ImageUpload;
